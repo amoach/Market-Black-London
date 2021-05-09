beeaad0ae6bfdf3eb2f1dcab6f389f85577820cd
amoach <amoach24@gmail.com>
2021-05-09 15:41:51 +0000

    add markers to map

--- assets/js/map2.js
+++ assets/js/map2.js
@@ -1,3 +1,4 @@
+// my marker locations
 const locations = [
   { lat: 51.469801, lng: -0.068252, 
     message: "The African brand incubator"}, //Bidhaar
@@ -12,12 +13,20 @@ const locations = [
   { lat: 51.549469, lng: -0.054751 }, //The Black Butcher
 ];
 
+// create map tiles provided by ArcGIS and Esri
 let mapTileLayers = L.tileLayer("http://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
     attribution: "Powered by <a href='https://developers.arcgis.com/terms/attribution/' target='_blank' rel='noopener'>Esri</a>"
 });
 
+// generate the map using the mapTileLayers above, using London as the centre
 let map = L.map("map", {
     layers: [mapTileLayers],
     center: [51.507351, -0.127758],
     zoom: 10
+});
+
+// loop over all locations from the "locations" array above
+locations.forEach(location => {
+    let marker = L.marker([location.lat, location.lng]).addTo(map);
+    marker.bindTooltip(location.message);
 });
