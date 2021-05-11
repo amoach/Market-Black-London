da9a8f192778e8d70bfdb848a9e491253c2b3956
amoach <amoach24@gmail.com>
2021-05-11 14:18:40 +0000

    read me

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(form) {
+    emailjs.send("gmail", "market", {
+        "from_name": form.name.value,
+        "from_email": form.emailaddress.value,
+        "marketblack_enquiry": form.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  
+}
